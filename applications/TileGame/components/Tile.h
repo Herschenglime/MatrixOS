0f66dfe49a8311f9357aa90f1b94350f7ad80536
Herschenglime <herschenglime@gmail.com>
2023-08-07 23:30:51 -0700

    updated color table

--- applications/TileGame/components/Tile.h
+++ applications/TileGame/components/Tile.h
@@ -13,13 +13,21 @@ class Tile {
   // the associated color of each rank (roygbiv for now)
   vector<Color> rankColors = {
     0x000000, 
-    0xFF0000, 
-    0xFF7F00,
-    0xFFFF00,
-    0x00FF00,
-    0x0000FF,
-    0x4B0082,
-    0x9400D3
+    0xFF0000,
+    0xFF8800,
+    0xFFC800,
+    0xF2FF00,
+    0x6AFF00,
+    0x00FFA2,
+    0x00FFE6,
+    0x00D9FF,
+    0x0095FF,
+    0x0051FF,
+    0x000DFF,
+    0x3700FF,
+    0x7700FF,
+    0xBB00FF,
+    0xFF00FF
     };
 
   void light();
